c3f1a031deaf6ee50643bcc39a421cc8137fb0fc
Maggie <zach.benton.27@gmail.com>
2023-05-12 10:45:05 -0400

    add modal and api usage

--- sql/steps.sql
+++ sql/steps.sql
@@ -1,26 +1,30 @@
 CREATE DATABASE reviews;
 \c reviews;
 CREATE TABLE users (
+    id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(50),
-    password VARCHAR(50),
-    datecreated DATE
+    password TEXT,
+    datecreated DATE,
+    email VARCHAR(50)
 );
 CREATE TABLE userreviews (
+    userid ibtjbdf,
+    movieid,
     movietitle VARCHAR(100),
     stars INTEGER,
     datereviewed DATE,
     reviewjson JSON
+    FOREIGN KEY (userid) REFERENCES users(id),
+    
 );
 
 CREATE TABLE movieinfo (
+    id INT
     movietitle VARCHAR(50),
     yearreleased INTEGER,
     totalstars INTEGER,
     userrevjson JSON
 );
-INSERT INTO users (username, password, datecreated)
-VALUES ('johndoe', 'mypassword', '2023-04-30');
-
 
 INSERT INTO userreviews (movietitle, stars, datereviewed, reviewjson)
 VALUES ('The Godfather', 5, '2023-04-30', '{"review": "This is a great movie!", "author": "johndoe"}');
