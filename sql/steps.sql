72624f2816e62d4ed4a597cf55049201aa56b3d3
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-05-12 10:45:08 -0400

    adding sql steps

--- sql/steps.sql
+++ sql/steps.sql
@@ -8,30 +8,24 @@ CREATE TABLE users (
     email VARCHAR(50)
    
 );
-CREATE TABLE userreviews (
-    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    userid INT REFERENCES users (id),
-    movietitle VARCHAR(100),
-    stars INTEGER,
-    datereviewed DATE,
-    reviewjson JSON,
-    FOREIGN KEY (userid) REFERENCES users(id) 
-    FOREIGN KEY (movieid) REFERENCES movieinfo(id)
-);
-
 CREATE TABLE movieinfo (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    movieid INTEGER,
     movietitle VARCHAR(50),
     yearreleased INTEGER,
     totalstars INTEGER,
-    userrevjson JSON,
+    userrevjson JSON
+);
+CREATE TABLE userreviews (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    movietitle VARCHAR(100),
+    movieid INTEGER,
+    stars INTEGER,
+    datereviewed DATE,
+    reviewjson JSON,
+    FOREIGN KEY (id) REFERENCES users(id), 
+    FOREIGN KEY (movieid) REFERENCES movieinfo(id)
 );
 
 
-INSERT INTO userreviews (movietitle, stars, datereviewed, reviewjson)
-VALUES ('The Godfather', 5, '2023-04-30', '{"review": "This is a great movie!", "author": "johndoe"}');
 
 
-INSERT INTO movieinfo ( movietitle, yearreleased, totalstars, userrevjson)
-VALUES ( 'The Godfather', 1972, 5, '{"reviews": [{"review": "This is a great movie!", "author": "johndoe"}]}');
